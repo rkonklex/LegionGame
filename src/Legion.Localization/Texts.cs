60e577a99a7bbac2dbda5f11b4efc93d7355d04c
Robert Konklewski <nythil@gmail.com>
2023-01-20 00:12:58 +0100

    Add support for Polish characters
    Include parts of Latin Extended-A and Latin Extended-B Unicode blocks
    in the generated SpriteFont to be able to draw Polish characters.
    The new defender2 font contains the needed glyphs!
    
    Removing Polish characters during localization is no longer necessary.

--- src/Legion.Localization/Texts.cs
+++ src/Legion.Localization/Texts.cs
@@ -43,66 +43,7 @@ namespace Legion.Localization
                 text = string.Format(text, args);
             }
 
-            //TODO: hack!!! current font doesn't support polish characters, for now we just remove them!
-            text = RemovePolishCharacters(text);
             return text;
-        }
-
-        private string RemovePolishCharacters(string text)
-        {
-            if (string.IsNullOrEmpty(text))
-            {
-                return text;
-            }
-
-            var normalizedArray = new char[text.Length];
-            for (var i = 0; i < text.Length; i++)
-            {
-                normalizedArray[i] = NormalizeChar(text[i]);
-            }
-            return new String(normalizedArray);
-        }
-
-        private char NormalizeChar(char c)
-        {
-            switch (c)
-            {
-                case 'ą':
-                    return 'a';
-                case 'Ą':
-                    return 'A';
-                case 'ć':
-                    return 'c';
-                case 'Ć':
-                    return 'C';
-                case 'ę':
-                    return 'e';
-                case 'Ę':
-                    return 'E';
-                case 'ł':
-                    return 'l';
-                case 'Ł':
-                    return 'L';
-                case 'ń':
-                    return 'n';
-                case 'Ń':
-                    return 'N';
-                case 'ó':
-                    return 'o';
-                case 'Ó':
-                    return 'O';
-                case 'ś':
-                    return 's';
-                case 'Ś':
-                    return 'S';
-                case 'ż':
-                case 'ź':
-                    return 'z';
-                case 'Ż':
-                case 'Ź':
-                    return 'Z';
-            }
-            return c;
         }
     }
 }
