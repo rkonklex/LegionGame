76c5742d789b7165e891c0333b8c5017686c3851
Robert Konklewski <nythil@gmail.com>
2025-01-31 13:48:30 +0100

    Fix MapMenu bounds and button positioning

--- src/Legion/Views/Map/Controls/MapMenu.cs
+++ src/Legion/Views/Map/Controls/MapMenu.cs
@@ -16,6 +16,7 @@ namespace Legion.Views.Map.Controls
 
         public MapMenu(IGuiServices guiServices) : base(guiServices)
         {
+            Bounds = new Rectangle(GuiServices.GameBounds.Width - 37, GuiServices.GameBounds.Height - 38, 32, 32);
             CreateElements();
         }
 
@@ -26,11 +27,11 @@ namespace Legion.Views.Map.Controls
         {
             _startButton = new Button(GuiServices, "Start");
             _startButton.Clicked += args => StartClicked?.Invoke(args);
-            _startButton.Bounds = new Rectangle(GuiServices.GameBounds.Width - 35, GuiServices.GameBounds.Height - 36, DefaultButtonWidth, DefaultButtonHeight);
+            _startButton.Bounds = new Rectangle(Position.X + 2, Position.Y + 2, DefaultButtonWidth, DefaultButtonHeight);
 
             _optionsButton = new Button(GuiServices, "Opcje");
             _optionsButton.Clicked += args => OptionsClicked?.Invoke(args);
-            _optionsButton.Bounds = new Rectangle(GuiServices.GameBounds.Width - 35, GuiServices.GameBounds.Height - 21, DefaultButtonWidth, DefaultButtonHeight);
+            _optionsButton.Bounds = new Rectangle(Position.X + 2, Position.Y + 17, DefaultButtonWidth, DefaultButtonHeight);
 
             _startButton.Center = _optionsButton.Center = true;
 
@@ -43,9 +44,7 @@ namespace Legion.Views.Map.Controls
 
         protected override void OnDraw()
         {
-            GuiServices.BasicDrawer.DrawBorder(Color.Red,
-                GuiServices.GameBounds.Width - 37,
-                GuiServices.GameBounds.Height - 38, 32, 32);
+            GuiServices.BasicDrawer.DrawBorder(Color.Red, Bounds);
         }
     }
 }
